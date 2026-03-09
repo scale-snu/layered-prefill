053f80e5201a7c0ab56e468e3d578907a2ca9cc3
gunjunlee <gunjunlee97@gmail.com>
2026-03-09 12:41:51 +0900

    chore: add license headers to remaining csrc files
    - csrc/custom_all_reduce.cuh, custom_all_reduce_kernels.cu: Apache-2.0 (vLLM)
    - csrc/moe_align_block_size_kernels.cu, store_kvcache_kernels.cu: Apache-2.0 (sarathi-serve)
    - csrc/reduction_utils.cuh: Apache-2.0 (sarathi-serve, alongside existing FasterTransformer attribution)

--- csrc/reduction_utils.cuh
+++ csrc/reduction_utils.cuh
@@ -1,4 +1,15 @@
 /*
+ * Adapted from https://github.com/microsoft/sarathi-serve
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/*
  * Adapted from
  * https://github.com/NVIDIA/FasterTransformer/blob/release/v5.3_tag/src/fastertransformer/kernels/reduce_kernel_utils.cuh
  * Copyright (c) 2023, The Sarathi team.
